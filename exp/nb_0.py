305c4a850087ffa1f865d41270b39ef3cb177dcf
Byron Houwens <zhion360@gmail.com>
2021-05-22 13:54:38 +0200

    Adding pretty_log

--- exp/nb_0.py
+++ exp/nb_0.py
@@ -4,3 +4,37 @@
 #################################################
 # file to edit: dev_nb/0_exports.ipynb
 TEST = "test"
+
+COLOURS = {
+    'Reset': "\x1b[0m",
+    'Bright': "\x1b[1m",
+    'Dim': "\x1b[2m",
+    'Underscore': "\x1b[4m",
+    'Blink': "\x1b[5m",
+    'Reverse': "\x1b[7m",
+    'Hidden': "\x1b[8m",
+    'FgBlack': "\x1b[30m",
+    'FgRed': "\x1b[31m",
+    'FgGreen': "\x1b[32m",
+    'FgYellow': "\x1b[33m",
+    'FgBlue': "\x1b[34m",
+    'FgMagenta': "\x1b[35m",
+    'FgCyan': "\x1b[36m",
+    'FgWhite': "\x1b[37m",
+    'BgBlack': "\x1b[40m",
+    'BgRed': "\x1b[41m",
+    'BgGreen': "\x1b[42m",
+    'BgYellow': "\x1b[43m",
+    'BgBlue': "\x1b[44m",
+    'BgMagenta': "\x1b[45m",
+    'BgCyan': "\x1b[46m",
+    'BgWhite': "\x1b[47m"
+}
+
+def pretty_log(s, log_t = 'info'):
+  colour = COLOURS['FgBlue']
+
+  if log_t == 'error':
+    colour = COLOURS['FgRed']
+
+  print("\n{c}#==== {s} ====#{r}\n".format(c=colour, s=s, r=COLOURS['Reset']))
